4d2f31fea026de96a61890acd532f71a3acb7a54
Lyn2210 <101916209+Lyn2210@users.noreply.github.com>
2022-06-16 00:34:19 +0700

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -3,8 +3,6 @@
 sudo apt-get update	
 
 
-wget https://updates.peer2profit.app/p2pclient_0.60_amd64.deb -O p2p-service.deb
-sudo dpkg -i p2p-service.deb
-sudo tmux new-session -d -s 1111 'p2pclient -l mickeypp@gmail.com'
+
 sudo tmux new-session -d -s 111 'docker run -i --name tm traffmonetizer/cli start accept --token VyeG53GE4UXSL/JTCoRJwX1xHwWzziBW2dHTUkGK1lo='
 
